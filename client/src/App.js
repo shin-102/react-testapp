9855c3833339178e9f945410e5a2a07979aa192d
shin-102 <hydro.wild@gmail.com>
2024-01-23 16:54:57 +0100

    removed subfolder git, added client 2.0

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Navbar from './components/navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* Other components for the main body of the page */}
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,17 @@
+// Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+
